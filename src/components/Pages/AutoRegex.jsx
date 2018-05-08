d2ba7e5a720353b4e85935b26f3a6aabe28961e3
entrptaher@gmail.com <entrptaher@gmail.com>
2018-05-09 04:46:04 +0600

    Add sample optimization poc

--- src/components/Pages/AutoRegex.jsx
+++ src/components/Pages/AutoRegex.jsx
@@ -1,39 +1,90 @@
-import React from 'react';
-import regexGen from 'autoregex'
-import autoBind from 'react-autobind';
+import React from "react";
+import regexGen from "autoregex";
+import autoBind from "react-autobind";
+import regexOptimizer from "regex-optimizations";
 
 class App extends React.Component {
   constructor() {
     super();
     autoBind(this);
     this.state = {
-      sampleText: "enter sample word or number like 123"
-    }
+      text: "enter sample word or number like 123",
+      regex: null,
+      optimized: null
+    };
+  }
+  componentDidMount() {
+    this.getRegexText(this.state.text);
   }
   getRegexText(text) {
     try {
-      if (!text) return "";
-      return regexGen(text).full;
+      if (!text) throw('Blank data')
+      const regex = regexGen(text).full;
+      const optimized = new regexOptimizer(new RegExp(regex));
+      this.setState({ regex, optimized: optimized.output.string });
     } catch (error) {
-      return ""
+      return this.setState({ regex: '', optimized: '' });
     }
   }
   changeSampleText(e) {
-    this.setState({ sampleText: e.target.value })
+    const text = e.target.value;
+    this.setState({ text });
+    this.getRegexText(text);
+    
   }
   render() {
-    return <div className='App'>
-      <div className="jumbotron">
-        <h1 className="display-5">Generate Regex Automatically!</h1>
-        <p className="lead">The regexGen() function generates a regex from a single word sample. By using single sample, the generated regex will not match words that are identical to the sample but will match words having the exact same properties like the sample's properties.</p>
-        <a className="btn btn-primary" href="https://github.com/entrptaher/autoregex" role="button" target="_blank">Learn more</a>
-        <a className="btn btn-secondary" href="https://github.com/KHTamim/Automated-Regular-Expression-Generation" role="button" target="_blank">Orignal Script Source</a>
-        <hr className="my-4" />
-        <h3 className="text-center">Testing Ground</h3>
-        <p><input type="text" className="form-control" placeholder="Sample Data" onChange={this.changeSampleText} value={this.state.sampleText} /></p>
-        <p className="sample-output">Sample Regex Output: <b>{this.getRegexText(this.state.sampleText)}</b></p>
+    return (
+      <div className="App">
+        <div className="jumbotron">
+          <h1 className="display-5">Generate Regex Automatically!</h1>
+          <p className="lead">
+            The regexGen() function generates a regex from a single word sample.
+            By using single sample, the generated regex will not match words
+            that are identical to the sample but will match words having the
+            exact same properties like the sample's properties.
+          </p>
+          <a
+            className="btn btn-primary"
+            href="https://github.com/entrptaher/autoregex"
+            role="button"
+            target="_blank"
+          >
+            Learn more
+          </a>
+          <a
+            className="btn btn-secondary"
+            href="https://github.com/KHTamim/Automated-Regular-Expression-Generation"
+            role="button"
+            target="_blank"
+          >
+            Orignal Script Source
+          </a>
+          <hr className="my-4" />
+          <h3 className="text-center">Testing Ground</h3>
+          <p>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Sample Data"
+              onChange={this.changeSampleText}
+              value={this.state.text}
+            />
+          </p>
+          <p className="sample-output well well-sm">
+            Sample Regex Output:
+            <div className="card bg-light p-3">
+              <p className="mb-0">{this.state.regex}</p>
+            </div>
+          </p>
+          <p className="sample-output well well-sm">
+            Sample Optimized Output:
+            <div className="card bg-light p-3">
+              <p className="mb-0">{this.state.optimized}</p>
+            </div>
+          </p>
+        </div>
       </div>
-    </div>
+    );
   }
 }
 export default App;
